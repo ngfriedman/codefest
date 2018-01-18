87f912782c2353070ab36b1dfe92c629daebdd1e
Timothy Stiles <tim@stiles.io>
2018-01-18 15:26:37 -0500

    added cv2 imread, imshow example with requirements.txt

--- codefest.py
+++ codefest.py
@@ -1,15 +1,8 @@
-import matplotlib.pyplot as plt
+import numpy as np
 import cv2
 
-cv2.imsave('cells.png', face)
-
 face = cv2.imread('cells.png')
-type(face)
-#'numpy.ndarray'
-face.shape, face.dtype
-#((768, 1024, 3), dtype('uint8'))
+cv2.imshow('image', face)
 
-plt.imshow(face, cmap=plt.cm.gray, vmin=30, vmax=200)
-plt.axis('off')
-#plt.contour(face)
-plt.show()
+cv2.waitKey(0)
+cv2.destroyAllWindows()
